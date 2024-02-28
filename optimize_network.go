6edbc5a56f26a18b56a4d15cef096467e32d87a3
Nick-Petruccelli <NickPetro18@gmail.com>
2024-02-28 11:46:40 -0500

    Json: Open file, Make map

--- optimize_network.go
+++ optimize_network.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import(
+	"fmt"
+	"os"
+	"io/ioutil"
+	"encoding/json"
+)
 
 func main()  {
-	fmt.Println("hello world!")
+	cap_graph_json, err := os.Open("capacity_graph.json")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("File opened successfully")
+
+	defer cap_graph_json.Close()
+
+	byte_value, _ := ioutil.ReadAll(cap_graph_json)
+
+	var res map[string]interface{}
+	json.Unmarshal([]byte(byte_value), &res)
+
+	fmt.Println(res["source1"])
 }
